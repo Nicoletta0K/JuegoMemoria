e58ad182deb7aa61a137dc14724213ce5552fff5
Nicoletta <nicoleartavia062@gmail.com>
2026-07-24 22:35:53 -0600

    Cronometro
    en el anterior commit de cronometro no aparece programado.

--- src/modelo/Cronometro.java
+++ src/modelo/Cronometro.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
-
-/**
- *
- * @author nicol
- */
-public class Cronometro {
-    
+    public class Cronometro {
+    private int segundos;
+    private boolean enEjecucion;
+    public Cronometro() {
+        reiniciar();
+    }
+    public void incrementar() {
+        if (enEjecucion) {
+            segundos++;
+        }
+    }
+    public void iniciar() {
+        enEjecucion = true;
+    }
+    public void detener() {
+        enEjecucion = false;
+    }
+    public void reiniciar() {
+        segundos = 0;
+        enEjecucion = false;
+    }
+    public int obtenerSegundos() {
+        return segundos;
+    }
+    public String obtenerTiempoFormateado() {
+        int minutos = segundos / 60;
+        int segs = segundos % 60;
+        return String.format("%02d:%02d", minutos, segs);
+    }
 }
+
