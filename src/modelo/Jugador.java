b7bb0f07720155f4aeed0e93bbde889d793c8be1
Maria <mafloresur@est.utn.ac.cr>
2026-07-24 18:00:33 -0600

    Se inicio la clase jugador que administra lo que hace jugador

--- src/modelo/Jugador.java
+++ src/modelo/Jugador.java
@@ -1,43 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package modelo;
 
+/**
+ *
+ * @author María J
+ */
 public class Jugador {
-    
     private int puntaje;
     private int intentos;
     private int parejasEncontradas;
-
+    private static final int PUNTOS_POR_PAREJA = 100;
+    private static final int PUNTOS_POR_INTENTO = 20;
     public Jugador() {
         this.puntaje = 0;
         this.intentos = 0;
         this.parejasEncontradas = 0;
     }
-
-    public void sumarPareja() {
-       puntaje += 100;
-    parejasEncontradas++;
-    }
-
-    public void sumarIntento() {
-         intentos++;
-    puntaje -= 20; 
-        
-    }
-
-    public void reiniciar() {
-          puntaje = 0;
-    intentos = 0;
-    parejasEncontradas = 0;
-    }
-
     public int getPuntaje() {
         return puntaje;
     }
-
     public int getIntentos() {
         return intentos;
     }
-
     public int getParejasEncontradas() {
         return parejasEncontradas;
+    }
+   
+    public void sumarPareja() {
+        parejasEncontradas++;
+        puntaje += PUNTOS_POR_PAREJA;
+    }
+   
+    public void sumarIntento() {
+        intentos++;
+        puntaje -= PUNTOS_POR_INTENTO;
+        if (puntaje < 0) {
+            puntaje = 0;
+        }
+    }
+    
+    public void reiniciar() {
+        puntaje = 0;
+        intentos = 0;
+        parejasEncontradas = 0;
     }
 }
